71cb57d9d4b573501cdf545c3226271022a654f1
lifei <372232197@qq.com>
2020-01-21 15:50:00 +0800

    补充模型映射数据库注意事项

--- api.py
+++ api.py
@@ -2,7 +2,8 @@ from flask import Flask
 import config
 from exts import db
 # 没有导入db下绑定的模型会报【INFO  [alembic.env] No changes in schema detected.】未检测到变化
-from models import User
+# 原理，manage文件在导入app的时候会检测到需要同步数据库的模型，所以也可以在manage.py创建manager对象前导入
+# from models import User
 app = Flask(__name__)
 app.config.from_object(config)
 
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from api import app
 from exts import db
 from flask_migrate import Migrate,MigrateCommand
-
+from models import User
 manager=Manager(app)
 # 绑定app和数据库
 Migrate(app,db)
