ad3a1b6cf6c3faa30ce7a5b1a7f0bec3c5638e6e
lifei <372232197@qq.com>
2020-01-22 10:16:37 +0800

    上传文件表单验证以及合并不可变字典

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 import re
 
 from flask import Flask,request,render_template,send_from_directory
+# 文件名安全检查
 from werkzeug.utils import secure_filename
-
+# 将两个不可变的字典合并起来
+from werkzeug.datastructures import CombinedMultiDict
+# 导入验证表单
+from forms import UploadForm
 import os
 UPLOAD_PATH=os.path.join(os.path.dirname(__file__),'static/images')
 
@@ -17,24 +21,28 @@ def upload():
     if request.method=='GET':
         return render_template('upload.html')
     else:
-        desc=request.form.get('desc')
-        avatar=request.files.get('avatar')
-        # 网络，安全隐患无处不在
-        # 对于文件名做安全检测，以防黑客攻击；linux ../../User/.bashrc，直接覆盖就哭鼻子了
-        # 对中文文件名支持的不好，会直接剔除掉，可以自行处理
-        origin_name=avatar.filename
-        # 处理的小demo
-        if re.match('[a-z]',origin_name):
-            filename=secure_filename(origin_name)
+        # 留意传参类型,将两个不可变打包起来，统一验证
+        form=UploadForm(CombinedMultiDict([request.files,request.form]))
+        if form.validate():
+            # 合并后提供了第二种获取数据的方式,验证通过后，在form中获取
+            # desc=request.form.get('desc')
+            # avatar=request.files.get('avatar')
+            desc = form.desc.data
+            avatar = form.avatar.data
+            # 网络，安全隐患无处不在
+            # 对于文件名做安全检测，以防黑客攻击；linux ../../User/.bashrc，直接覆盖就哭鼻子了
+            # 对中文文件名支持的不好，会直接剔除掉，可以自行处理
+            origin_name=avatar.filename
+            # 处理的小demo
+            if re.match('[a-z]',origin_name):
+                filename=secure_filename(origin_name)
+            else:
+                filename=origin_name
+            avatar.save(os.path.join(UPLOAD_PATH,filename))
+            return desc
         else:
-            filename=origin_name
-        avatar.save(os.path.join(UPLOAD_PATH,filename))
-        return desc
-
-# 下载/获取图片
-@app.route('/download/<filename>/')
-def download(filename):
-    return send_from_directory(UPLOAD_PATH,filename)
+            print(form.errors)
+            return 'no'
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,22 +1,9 @@
-from wtforms import Form,StringField,IntegerField,ValidationError,BooleanField,SelectField
-from wtforms.validators import Length,EqualTo,Email,InputRequired,NumberRange,Regexp,URL,UUID
+from wtforms import Form,FileField,StringField
+from wtforms.validators import InputRequired
 
-class RegistForm(Form):
-    # 导入验证器/处理条件,铲除一坨垃圾代码
-    # Length注意使用关键字传参，避免导致At least one of `min` or `max` must be specified
-    # validators接收验证列表
-    user=StringField(validators=[Length(min=3,max=10,message='长度必须介于3-10之间')])
-    # 使用【validate_字段名】来进行自定义验证器，会被自动识别来执行，不符合条件可以抛出异常；常用于验证码captcha
-    def validate_user(self,field):
-        if field.data !='hero':
-            raise ValidationError('用户输入错误')
+from flask_wtf.file import FileRequired,FileAllowed
+class UploadForm(Form):
+    # 黑客喜欢在上传的文件上做文章，验证文件上传是否为空，已经指定格式
+    avatar=FileField('肖像',validators=[FileRequired(),FileAllowed(['jpg','png','gif'])])
+    desc=StringField('描述',validators=[InputRequired()])
 
-    password=StringField(validators=[Length(min=6,max=10)])
-    password_repeat=StringField(validators=[Length(min=6,max=10),EqualTo('password',message='两次密码不一致')])
-
-class SettingForm(Form):
-    # 第一个参数用来指定label
-    user=StringField('用户名：',validators=[IntegerField()])
-    tags=SelectField('标签',choices=[('1','python'),('2','java')])
-    # 用户勾选，不需要验证
-    remember=BooleanField('记住我：')
