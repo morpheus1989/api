57368c1a70df9a4378e014997c654b2eb45fa1ef
lifei <372232197@qq.com>
2020-01-23 15:08:55 +0800

    sessionc操作和有效期设置

--- api.py
+++ api.py
@@ -1,19 +1,33 @@
-from flask import Flask,request,Response
-from datetime import datetime,timedelta
+from flask import Flask,request,session
+from datetime import timedelta
+import os
 import config
 app = Flask(__name__)
 app.config.from_object(config)
-
+# 加盐操作，随机数
+app.config['SECRET_KEY']=os.urandom(24)
+# 通过配置来修改session存储时间，2小时
+app.config['PERMANENT_SESSION_LIFETIME']=timedelta(hours=2)
 @app.route('/')
 def hello_world():
-    resp=Response('blog index')
-    # 设置cookie
-    resp.set_cookie('user','spider')
-    expires=datetime.now()+timedelta(days=1)
-    # 主域名前面加个点
-    resp.set_cookie('work','18',expires=expires,domain='.blog.com')
-    return resp
+    # 设置session
+    session['user']='杨过'
+    # permanent，持久化；设置成true默认存储31天
+    # 默认该参数为false，浏览器关闭失效
+    session.permanent=True
+    return 'hey boy!'
 
+@app.route('/get/')
+def get_session():
+    user=session.get('user')
+    return user or 'no find session'
+@app.route('/del/')
+def del_session():
+    # pop不存在的键会有异常
+    session.pop('user')
+    # 删除所有session
+    session.clear()
+    return 'delete success'
 
 if __name__ == '__main__':
     app.run()
