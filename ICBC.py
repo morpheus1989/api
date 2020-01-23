1be2b79db703b65d8dc8608a56a0b8cb24ce49e1
lifei <372232197@qq.com>
2020-01-23 19:59:43 +0800

    转账功能实现

--- ICBC.py
+++ ICBC.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, views, request
+from flask import Flask, render_template, views, request, session
 import config
-from forms import RegistForm
+from forms import RegistForm, LoginForm, TransferForm
 from exts import db
 from models import User
+from auth import login_required
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -35,7 +36,57 @@ class RegistView(views.MethodView):
             print(form.errors)
             return '注册失败'
 
+
+class LoginView(views.MethodView):
+    def get(self):
+        return render_template('login.html')
+
+    def post(self):
+        form = LoginForm(request.form)
+        if form.validate():
+            email = form.email.data
+            pwd = form.pwd.data
+            user = User.query.filter(User.email == email, User.pwd == pwd).first()
+            if user:
+                session['user_id'] = user.id
+                return '登录成功'
+            else:
+                return '邮箱或者密码错误'
+
+        return '邮箱或者密码错误'
+
+
+class TransferView(views.MethodView):
+    decorators=[login_required]
+    def get(self):
+        return render_template('transfer.html')
+
+    def post(self):
+        form = TransferForm(request.form)
+        if form.validate():
+            email = form.email.data
+            money = form.money.data
+            user = User.query.filter_by(email=email).first()
+            if user:
+                user_id = session.get('user_id')
+                print(user_id)
+                myself = User.query.get(user_id)
+                if myself.deposit >= money:
+                    user.deposit += money
+                    myself.deposit -= money
+                    db.session.commit()
+                    return '转账成功'
+                else:
+                    return '余额不足'
+            else:
+                return '用户不存在'
+
+        return '邮箱或者金额错误'
+
+
 app.add_url_rule('/regist/', view_func=RegistView.as_view('regist'))
+app.add_url_rule('/login/', view_func=LoginView.as_view('login'))
+app.add_url_rule('/transfer/', view_func=TransferView.as_view('transfer'))
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,8 +1,30 @@
-from wtforms import Form,StringField,FloatField
-from wtforms.validators import Email,Length,EqualTo,InputRequired
+from wtforms import Form, StringField, FloatField
+from wtforms.validators import Email, Length, EqualTo, InputRequired,NumberRange
+from models import User
+
+
 class RegistForm(Form):
-    email=StringField(validators=[Email()])
-    user=StringField(validators=[Length(3,20)])
-    pwd=StringField(validators=[Length(6,20)])
-    pwd_repeat=StringField(validators=[EqualTo('pwd')])
-    deposit=FloatField(validators=[InputRequired()])
+    email = StringField(validators=[Email()])
+    user = StringField(validators=[Length(3, 20)])
+    pwd = StringField(validators=[Length(6, 20)])
+    pwd_repeat = StringField(validators=[EqualTo('pwd')])
+    deposit = FloatField(validators=[InputRequired()])
+
+
+class LoginForm(Form):
+    email = StringField(validators=[Email()])
+    pwd = StringField(validators=[Length(6, 20)])
+    # def validate(self):
+    #     result=super(LoginForm, self).validate()
+    #     if not result:
+    #         return False
+    #     email=self.email.data
+    #     pwd=self.pwd.data
+    #     user=User.query.filter(User.email==email,User.pwd==pwd).first()
+    #     if not user:
+    #         return self.email.errors.append('邮箱或者密码错误')
+    #     return True
+class TransferForm(Form):
+    email = StringField(validators=[Email()])
+    # 表单验证会自动将前端提交的文本数据转换成对应类型
+    money = FloatField(validators=[NumberRange(1,100000)])
