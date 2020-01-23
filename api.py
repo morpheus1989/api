29d0631c05f3371b785158057cee46a71c60be83
lifei <372232197@qq.com>
2020-01-23 13:35:12 +0800

    添加删除cookie

--- api.py
+++ api.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask,request,Response
+from datetime import datetime,timedelta
 import config
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route('/')
 def hello_world():
-    return 'you gey Hello World!'
+    resp=Response('Hello World!')
+    # 设置cookie
+    resp.set_cookie('user','spider')
+    expires=datetime.now()+timedelta(days=1)
+    resp.set_cookie('work','18',expires=expires)
 
+    return resp
+
+@app.route('/del/')
+def del_cookie():
+    resp=Response('ok')
+    # 删除cookie
+    resp.delete_cookie('user')
+    return resp
 
 if __name__ == '__main__':
     app.run()
