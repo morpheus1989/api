9ef10fe8ca53ce11222decad462b0e88050f4182
lifei <372232197@qq.com>
2020-01-23 14:13:28 +0800

    设置cookie可以在子域名有效使用

--- api.py
+++ api.py
@@ -6,20 +6,14 @@ app.config.from_object(config)
 
 @app.route('/')
 def hello_world():
-    resp=Response('Hello World!')
+    resp=Response('blog index')
     # 设置cookie
     resp.set_cookie('user','spider')
     expires=datetime.now()+timedelta(days=1)
-    resp.set_cookie('work','18',expires=expires)
-
+    # 主域名前面加个点
+    resp.set_cookie('work','18',expires=expires,domain='.blog.com')
     return resp
 
-@app.route('/del/')
-def del_cookie():
-    resp=Response('ok')
-    # 删除cookie
-    resp.delete_cookie('user')
-    return resp
 
 if __name__ == '__main__':
     app.run()
