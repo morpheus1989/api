97cc594f809ce4d4c2f1613cf5b7f8454340f541
lifei <372232197@qq.com>
2020-01-24 22:18:25 +0800

    钩子函数上

--- api.py
+++ api.py
@@ -1,46 +1,34 @@
-from flask import Flask,request,current_app,url_for,g
+from flask import Flask,request,g,session
 import config
-from logs import log
 app = Flask(__name__)
 app.config.from_object(config)
-# 先看结果后讲原理，内容太抽象吃不消
-
-# 手动创建上下文，将其加入栈中
-# app_content=app.app_context()
-# app_content.push()
-# print(current_app.name)
-
-# with语句内部也可以拥有app上下文
-# with app.app_context():
-#     print(current_app.name)
-
-# 网络请求会自动创建应用上下文
 @app.route('/')
 def hello_world():
-    user=request.args.get('user')
-    # g对象在flask应用运行期间都可以使用，也是线程隔离的
-    # 专门存储开发者自己定义的一些数据，方便其他地方调用
-    # 无需传参，更加方便
-    g.user=user
-    log()
-    return user or 'no user'
+    if hasattr(g,'user'):
+        print(g.user)
+    return 'hello'
 
 @app.route('/list/')
 def list():
-    return 'list log'
-# 在视图函数中，不用担心上下文问题，因为通过url访问的方式执行时，
-# flask底层自动会给栈中推入上下文
-# app和请求分别有一个堆栈【localstack】；local表明对象具有线程隔离的特性
-# url_for 先获取app上下文，然后检查请求上下文；
-# app/request的相关操作都需要上下文支持
-# 使用test_request_context自动创建请求上下文时，先检查应用上下文，没有就自动推入一个；
-with app.test_request_context():
-    print(url_for('list'))
+    session['user_id']=1
+    return 'list'
 
-# 疑问，为什么要把上下文推入栈中，直接创建上下文不好么？？
-# 因为在flask中不仅仅只创建一个app，多个app使用栈来分别管理
-# 当前app或者正在使用的app，肯定在栈顶【top】，用完出栈【从栈顶部移除】就行
-# 同理，测试代码/离线脚本/多个视图，需要创建多个请求上下文
+# 第一次请求之前执行
+# @app.before_first_request
+# def first_request():
+#     print('here is right')
+
+# 钩子函数是使用特定的装饰器装饰的函数，形象地指将目标钩住
+# 为何称为钩子函数？因为它可以在正常执行的代码中，插入一段自己想要执行的代码
+# hook破解软件/操作系统
+# 每次请求之前先执行，通常将后面多处用到的数据处理好，简化视图操作
+@app.before_request
+def before_req():
+    user_id = session.get('user_id')
+    if user_id:
+        g.user='叶问'
+
+
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-host, port, user, password, db = '127.0.0.1', '3306', 'root', '123456', 'flask_tool'
-DB_CONFIG = dict(host=host, port=port, user=user, password=password, database=db)
-DB_URI = 'mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8'.format(**DB_CONFIG)
+import os
 
-SQLALCHEMY_DATABASE_URI=DB_URI
-
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+SECRET_KEY=os.urandom(24)
