e1ac5b471eda3a68c8b1447e86ad84a9696ecab6
lifei <372232197@qq.com>
2020-01-26 11:49:09 +0800

    flask-restful基本使用

--- api.py
+++ api.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask,url_for
 import config
+
+from flask_restful import Api,Resource
 app = Flask(__name__)
 app.config.from_object(config)
+
+# 使用Api绑定app，返回api对象
+api=Api(app)
+
+class LoginView(Resource):
+    # def post(self):
+    #     return 'post ok'
+    def post(self,user=None):
+        return dict(user=user)
+
+api.add_resource(LoginView,'/login/<user>/','/regist/')
+
+with app.test_request_context():
+    # print(url_for('login'))
+    print(url_for('loginview',user='hero'))
 
 @app.route('/')
 def hello():
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
